44e2e7356a718b8a4a91445c6d99e5f077fd8e91
sphantix <sphantix@gmail.com>
2015-12-30 11:30:09 +0800

    Develop | Remove useless empty directories and modify install script

--- install.sh
+++ install.sh
@@ -8,4 +8,6 @@ cp ./.bashrc ~/ -rf
 cp ./.bash_aliases ~/ -rf
 cp ./.tool/ ~/ -rf
 cp ./.gitconfig ~/ -rf
+#Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "complete!"
