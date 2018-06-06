898cfcdc0dd82a4bc35c0841d3ce291dd6f23320
Xu Hang <hangxu@antiy.cn>
2018-06-06 14:44:50 +0800

    auto install go

--- install.sh
+++ install.sh
@@ -4,11 +4,16 @@
 os=`uname`
 mac_bash_profile="$HOME/.bash_profile"
 
+make_dirs() {
+    mkdir /home/sphantix/go
+}
+
 copy_file() {
     cp -rf ./.vim ~/
     cp -rf ./.vimrc ~/
     cp -rf ./.bashrc ~/
     cp -rf ./.bash_aliases ~/
+    cp -rf ./.bash_environment ~/
     cp -rf ./.tool ~/
     cp -rf ./.gitconfig ~/
     cp -rf ./.spacemacs.d/ ~/
@@ -40,18 +45,37 @@ git_init() {
 
 install_softwares() {
     sudo apt update
+    #c
     sudo apt-get install ctags
     sudo apt-get install cscope
+    sudo apt-get install clang
+    #python
     sudo apt install python3-dev python3-pip
+    #go
+    sudo apt install golang-go
+    go get -u -v github.com/nsf/gocode
+    go get -u -v github.com/rogpeppe/godef
+    go get -u -v golang.org/x/tools/cmd/guru
+    go get -u -v golang.org/x/tools/cmd/gorename
+    go get -u -v golang.org/x/tools/cmd/goimports
+    go get -u -v github.com/alecthomas/gometalinter
+    gometalinter --install --update
+    #thefuck
     sudo pip3 install thefuck
+    #proxychains
+    sudo apt install proxychains
 }
 
 # main routines
 echo "install all config.."
+# make dirs
+make_dirs
 # copy files & dirs
 copy_file
 # git clone
 git_init
+# update environments
+source ~/.bashrc
 # install softwares
 install_softwares
 
